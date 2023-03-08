c037fb4fa957baebd346880e6aba77c07fb2171f
Hypsster <work@hypsster.com>
2023-03-08 10:21:26 -0500

    n

---
+++ src/add_numbers.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+
+public class add_numbers
+{
+    public static void main(String[] args)
+    {
+        Scanner userInput = new Scanner(System.in);
+
+        System.out.println("1st Number: ");
+        double num1 = userInput.nextDouble();
+        System.out.println("2nd Number: ");
+        double num2 = userInput.nextDouble();
+        double r = num1 + num2;
+
+        System.out.println("The total is: " +  r);
+
+
+
+
+
+
+
+
+
+    }
+
+}
---
+++ src/evenodd_checker.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class evenodd_checker {
+
+    public static void main(String[] args)
+    {
+        Scanner userInput = new Scanner(System.in);
+
+        int a = userInput.nextInt();
+
+        if (a % 2 == 0)
+        {
+            System.out.println(a + " is an even number");
+        }else{
+            System.out.println(a + " is an odd number");
+        }
+    }
+}
